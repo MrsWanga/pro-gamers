b6c4c45b7881106bd98cd9df7e1abf9c5fefed68
Patrycja <patrycja.wanga@gmail.com>
2022-11-07 22:51:54 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const nav = document.querySelector('.navbar');
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navToggler = document.querySelector('.navbar-toggler')
+    const navList = document.querySelector('.navbar-collapse');
+
+    function addShadow() {
+        if(window.scrollY>=300) {
+            nav.classList.add('shadow-bg');
+        }else {
+            nav.classList.remove('shadow-bg');
+        }
+    }
+
+    function addShadowToggler() {
+        if(navList.classList.contains('show')) {
+            nav.classList.add('shadow-bg');
+        }else {
+            nav.classList.remove('shadow-bg');
+        }
+    }
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+    window.addEventListener('scroll', addShadow)
+    navToggler.addEventListener('click', addShadowToggler)
+})
